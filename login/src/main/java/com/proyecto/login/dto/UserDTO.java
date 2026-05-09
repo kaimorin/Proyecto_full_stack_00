132641862fa92cfa072f569b067bab3bef8332d4
yva <yva.munoz@duocuc.cl>
2026-05-09 19:06:07 -0400

    comit dto-user

---
+++ login/src/main/java/com/proyecto/login/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package com.proyecto.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDTO {
+    private long id;
+    private String username;
+
+}
---
+++ login/src/main/java/com/proyecto/login/model/User.java
@@ -0,0 +1,17 @@
+package com.proyecto.login.model;
+
+
+import jakarta.persistence.*;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+}
