15d00d7a7772d4715021d427c455f93955c636dc
Nessa <va.roberson@duocuc.cl>
2026-05-16 03:44:27 -0400

    application properties creado (actualizar db)

--- hojadevida/src/main/java/com/proyecto/hojadevida/repository/HojaDeVidaRepository.java
+++ hojadevida/src/main/java/com/proyecto/hojadevida/repository/HojaDeVidaRepository.java
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.proyecto.hojadevida.model.HojaDeVida;
 
 public interface HojaDeVidaRepository extends JpaRepository<HojaDeVida, Long> {
-}
+} 
