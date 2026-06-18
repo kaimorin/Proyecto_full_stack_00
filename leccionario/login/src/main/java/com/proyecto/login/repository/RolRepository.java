3a8df495e62026161d9fb2806916be6d11e722d1
yva <yva.munoz@duocuc.cl>
2026-06-18 12:25:57 -0400

    nose323

--- leccionario/login/src/main/java/com/proyecto/login/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.login.repository;
-
-import com.proyecto.login.model.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
